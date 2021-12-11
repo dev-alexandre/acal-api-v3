f34a03ffd3d5d2a348b3e0ab5abc93f10dc843fa
Alexandre Queiroz <dev.alexandrequeiroz@gmail.com>
2021-12-11 10:45:50 -0300

    Lint

--- src/main/kotlin/br/com/acalv3/domain/repository/v3/PlaceRepository.kt
+++ src/main/kotlin/br/com/acalv3/domain/repository/v3/PlaceRepository.kt
@@ -3,5 +3,4 @@ package br.com.acalv3.domain.repository.v3
 import br.com.acalv3.domain.model.v3.PlaceModel
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface PlaceRepository : JpaRepository<PlaceModel, Long>{
-}
+interface PlaceRepository : JpaRepository<PlaceModel, Long>
--- src/test/kotlin/br/com/acalv3/integration/AddressTypeGatewayTest.kt
+++ src/test/kotlin/br/com/acalv3/integration/AddressTypeGatewayTest.kt
@@ -2,8 +2,12 @@ package br.com.acalv3.integration
 
 import br.com.acalv3.acalapiv3.builder.AddressTypeDataBuilder
 import br.com.acalv3.domain.gateway.v3.AddressTypeGateway
+import br.com.acalv3.domain.model.v3.AddressTypeModel
 import br.com.acalv3.domain.service.v3.AddressTypeService
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.runner.RunWith
@@ -18,8 +22,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
 @RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-//@AutoConfigureMockMvc
-//@WebMvcTest(HealthGateway::class)
 internal class AddressTypeGatewayTest {
 
 	companion object{
@@ -31,6 +33,9 @@ internal class AddressTypeGatewayTest {
 	@Autowired
 	lateinit var addressTypeService: AddressTypeService
 
+	@Autowired
+	lateinit var objectMapper: ObjectMapper
+
 	@BeforeEach
 	fun setup() {
 		mockMvc = MockMvcBuilders
@@ -40,16 +45,39 @@ internal class AddressTypeGatewayTest {
 	}
 
 	@Test
+	fun `Should save address type`() {
+
+		val addressTypeModelAsJson = objectMapper.writeValueAsString(
+			AddressTypeDataBuilder.build{
+			id = null
+			}
+		)
+
+		val result = mockMvc.perform(
+			MockMvcRequestBuilders
+				.post(URL)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(addressTypeModelAsJson)
+		).andExpect(MockMvcResultMatchers.status().isOk).andReturn()
+
+		val addressTypeModelAsObject = objectMapper.readValue<AddressTypeModel>(result.response.contentAsString)
+
+		assertNotNull(addressTypeModelAsObject.id)
+
+	}
+
 	fun `when Valid Input then Returns 200`() {
 		val addressTypeModel = AddressTypeDataBuilder.build()
 
 		val result = mockMvc.perform(
 			MockMvcRequestBuilders
 				.get("$URL/get/${addressTypeModel.id}")
+				.contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON)
 		).andExpect(MockMvcResultMatchers.status().isOk).andReturn()
 
 		assertEquals(result.response.contentAsString, "ok")
 	}
+
 
 }
